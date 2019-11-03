8c49306bb68114bbd6474f31fafb0dd993db63ac
bashayer_nouri <bashayer_nouri@hotmail.com>
2019-11-03 09:27:39 +0300

    list fixes

--- src/redux/actions/recipes.js
+++ src/redux/actions/recipes.js
@@ -1,7 +1,17 @@
-import { FETCH_RECIPES } from "./actionTypes";
+import { FETCH_RECIPES} from "./actionTypes";
 import instance from "./instance";
 
-export const fetchRecipes = ingredientsItems => async dispatch => {
+export const fetchRecipes = () => async dispatch => {
+  try {
+    const res = await instance.get("recipes/");
+    const recipes = res.data;
+    dispatch({ type: FETCH_RECIPES, payload: recipes });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchRecipesByIngredients = ingredientsItems => async dispatch => {
   try {
     const res = await instance.post("recipes/ingredients/", ingredientsItems);
     const recipes = res.data;
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,5 @@
 import { createStore, compose, applyMiddleware } from "redux";
-import { fetchIngredients, checkForExpiredToken } from "./actions";
+import { fetchIngredients, fetchRecipes, checkForExpiredToken } from "./actions";
 import reducer from "./reducers";
 import thunk from "redux-thunk";
 
@@ -8,6 +8,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 store.dispatch(checkForExpiredToken());
+store.dispatch(fetchRecipes());
 store.dispatch(fetchIngredients());
 
 export default store;
