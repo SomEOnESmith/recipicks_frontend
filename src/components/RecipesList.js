a658e1f1a951a07206505a8ffcad25efc3fb8c53
colkman <h.ali.alhaddad@hotmail.com>
2019-11-04 14:19:47 +0300

    Display multiple lists of recipes

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -6,16 +6,43 @@ import RecipeCard from "./RecipeCard";
 
 class RecipesList extends Component {
   render() {
-    const RecipeCards = this.props.recipes.map(recipe => (
+    console.log("TCL: RecipesList -> render -> recipes", this.props.recipes);
+    const exact = this.props.recipes.exact_match.map(recipe => (
       <RecipeCard key={recipe.id} recipe={recipe} />
     ));
-
+    const excess = this.props.recipes.user_has_excess_ingredients.map(
+      recipe => <RecipeCard key={recipe.id} recipe={recipe} />
+    );
+    const missing = this.props.recipes.user_has_missing_ingredients.map(
+      recipe => <RecipeCard key={recipe.id} recipe={recipe} />
+    );
     return (
-
       <div id="recipe-list" className="container">
         <div className="row" id="card-row">
-
-          {RecipeCards}
+          {exact.length !== 0 && (
+            <>
+              <h3 style={{ color: "yellow" }}>
+                I have the exact amount of ingredients:
+              </h3>
+              {exact}
+            </>
+          )}
+          {excess.length !== 0 && (
+            <>
+              <h3 style={{ color: "yellow" }}>
+                I have an excess amount of ingredients:
+              </h3>
+              {excess}
+            </>
+          )}
+          {missing.length !== 0 && (
+            <>
+              <h3 style={{ color: "yellow" }}>
+                I am missing some ingredients:{" "}
+              </h3>
+              {missing}
+            </>
+          )}
         </div>
       </div>
     );
