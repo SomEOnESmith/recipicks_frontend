c1be28dafa4dea359a2e5a177946513122e11648
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-11-07 15:16:24 +0300

    Minor fixes

--- src/components/ListPage/RecipesList.js
+++ src/components/ListPage/RecipesList.js
@@ -6,7 +6,6 @@ import RecipeCard from "./RecipeCard";
 
 class RecipesList extends Component {
   render() {
-
     const { recipes } = this.props;
     if (Array.isArray(recipes)) {
       const list = recipes.map(recipe => (
@@ -20,35 +19,21 @@ class RecipesList extends Component {
         </div>
       );
     } else {
-      const exact = recipes.perfect_match.map(recipe => (
-        <RecipeCard type={"exact"} key={recipe.id} recipe={recipe} />
+      const perfect = recipes.perfect_match.map(recipe => (
+        <RecipeCard type={"perfect"} key={recipe.id} recipe={recipe} />
       ));
-      const excess = recipes.user_excess_ings.map(recipe => (
+      const excess = recipes.user_excess_ingrs.map(recipe => (
         <RecipeCard type={"excess"} key={recipe.id} recipe={recipe} />
       ));
-      const missing = recipes.user_missing_ings.map(recipe => (
+      const missing = recipes.user_missing_ingrs.map(recipe => (
         <RecipeCard type={"missing"} key={recipe.id} recipe={recipe} />
       ));
       return (
         <div id="recipe-list" className="container">
           <div className="row" id="card-row">
-            {exact.length !== 0 && (
-              <>
-             
-                {exact}
-              </>
-            )}
-            {excess.length !== 0 && (
-              <>
-               
-                {excess}
-              </>
-            )}
-            {missing.length !== 0 && (
-              <>
-                {missing}
-              </>
-            )}
+            {perfect.length !== 0 && <>{perfect}</>}
+            {excess.length !== 0 && <>{excess}</>}
+            {missing.length !== 0 && <>{missing}</>}
           </div>
         </div>
       );
